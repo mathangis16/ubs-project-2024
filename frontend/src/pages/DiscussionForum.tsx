bcb9558e062a8a65cf61090476733a6f897fdb2d
mathangis16 <mathangis16@gmail.com>
2024-06-10 01:21:06 +0800

    minor discussion forum changes

--- frontend/src/pages/DiscussionForum.tsx
+++ frontend/src/pages/DiscussionForum.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const DiscussionForum = () => {
-    return <div> Discussion forum </div>;
+    const handleButtonClick = () => {
+        window.location.href = 'http://localhost:4000'; 
+    };// URL of the React application
+    return (
+    <div> 
+        <h1>Click here to go to the discussion forum!</h1>
+      <button onClick={handleButtonClick}>Go to React App</button>
+    </div>);
 };
 
 export default DiscussionForum;
